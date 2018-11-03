0e455fb127d97463ef625125ed40955a6cea6d57
Damien alias Dough <dough29@gmail.com>
2018-11-03 15:37:41 +0100

    Cleaning merge bugfix-2.0.x

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -828,9 +828,6 @@
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 20
 
-// Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
-
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
 
